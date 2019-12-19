f9afb130800c93c426006948a9f97b13cad0ec57
ZhengKeli <zhengkeli2009@126.com>
2019-12-19 22:28:01 +0300

    将原测试文件作为主运行程序main.mlx，跑成了mask部分

--- lines_mask.m
+++ lines_mask.m
@@ -1,27 +1,29 @@
-% 一般设置thickness=1, expand=3
 function [line1, line2] = lines_mask(p1, p2, ps, thickness, expand)
+% 一般设置thickness=1, expand=3
+
 dp = p2 - p1;
 l = sqrt(sum(dp.^2));
 n = dp / l;
 m = n * [0 -1;1 0];
-d1 = abs(sum((ps-p1).*n));
-d2 = abs(sum((ps-p2).*n));
-d1 = abs(sum((ps-p1).*m));
+
+d1 = abs(sum( (ps-reshape(p1,1,1,2)) .* reshape(n,1,1,2), 3));
+d2 = abs(sum( (ps-reshape(p2,1,1,2)) .* reshape(n,1,1,2), 3));
+dm = abs(sum( (ps-reshape(p1,1,1,2)) .* reshape(m,1,1,2), 3));
 
 line1 = thickness / 2 - d1 + expand;
 line1(line1 <= 0.0) = 0.0;
 line1(line1 >= expand) = expand;
 line1 = line1 / expand;
+
 line2 = thickness / 2 - d2 + expand;
 line2(line2 <= 0.0) = 0.0;
 line2(line2 >= expand) = expand;
 line2 = line2 / expand;
 
-cut = 1 - d1 + expand;
+cut = l - dm + expand;
 cut(cut <= 0.0) = 0.0;
 cut(cut >= expand) = expand;
 cut = cut / expand;
 
 line1 = line1.*cut;
 line2 = line2.*cut;
-end
