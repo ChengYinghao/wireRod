bda96cff938141bbfdee16809529ef9457ebb94c
ZhengKeli <zhengkeli2009@126.com>
2019-12-22 14:41:55 +0300

    将计算初始点的代码整理成函数了

--- initial_points.m
+++ initial_points.m
@@ -1,4 +1,39 @@
-function [p1, p2] = initial_points(theta1, rho1, theta2, rho2, width, height)
+function [p1, p2] = initial_points(edges, varargin)
+% 计算初始位置
+% 默认参数：
+% shrink = floor(min([width, height]/64));
+
+shrinking = nan;
+bin_threshold = 0.03;
+for i=2:2:length(varargin)
+    name = varargin{i-1};
+    value = varargin{i};
+    switch name
+        case "shrink"
+            shrinking = value;
+        case "bin_threshold"
+            bin_threshold = value;
+    end
+end
+
+[height, width] = size(edges);
+
+if isnan(shrinking)
+    shrinking = floor(min([width, height]/64));
+end
+edges_shrinked = edges(1:shrinking:end,1:shrinking:end);
+%figure; imshow(edges_shrinked,"InitialMagnification",shrinking*100), hold on
+
+[theta1, rho1_shrinked, theta2, rho2_shrinked] = find_parallel(edges_shrinked,bin_threshold);
+
+%img_shrinked = img(1:shrink_scale:end,1:shrink_scale:end,:);
+%figure; imshow(img_shrinked,"InitialMagnification",shrinking*100), hold on
+%plot_line(width,height,theta1,rho1_shrinked);
+%plot_line(width,height,theta2,rho2_shrinked);
+
+rho1 = rho1_shrinked * shrinking;
+rho2 = rho2_shrinked * shrinking;
+
 theta = (theta1 + theta2) / 2;
 rho = (rho1 + rho2) / 2;
 
@@ -7,7 +42,6 @@ cos_theta = cos(theta);
 
 xc = width / 2;
 yc = height / 2;
-
 xpc = xc * sin_theta * sin_theta + cos_theta * (rho - yc * sin_theta);
 ypc = yc * cos_theta * cos_theta + sin_theta * (rho - xc * cos_theta);
 
@@ -16,5 +50,3 @@ n = [cos_theta, sin_theta];
 d = (rho1 - rho2) / 2;
 p1 = pc + n * d;
 p2 = pc - n * d;
-end
-
